c7a1e88c872331170a2c7475123066ee7ca2eb02
Rystambek <rustambekjumaqulov@gmail.com>
2023-02-12 15:57:07 +0500

    getUpdates

---
+++ getUpdates.py
@@ -0,0 +1,11 @@
+import requests
+from pprint import pprint
+
+def getUpdates()->dict:
+    TOKEN = '5455890832:AAGh9-yUlkri7iXD5V4AOlWDdRN-pAiOBLY'
+    basic_url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+    response = requests.get(url=basic_url)
+    data = response.json()
+    return data
+
+pprint(getUpdates())

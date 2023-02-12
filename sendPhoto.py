fdc2de20e669caf96d59f336006d546e0df2e205
Rystambek <rustambekjumaqulov@gmail.com>
2023-02-12 19:52:55 +0500

    photo id

--- sendPhoto.py
+++ sendPhoto.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 TOKEN = '5455890832:AAGh9-yUlkri7iXD5V4AOlWDdRN-pAiOBLY'
 
-def sendPhoto(chat_id:int, photo_url:str):
+def sendPhoto(chat_id:int, photo:str):
     Basic_url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
     params = {
         'chat_id':chat_id,
@@ -15,7 +15,8 @@ def sendPhoto(chat_id:int, photo_url:str):
     return data
 
 photo_url = 'https://random.dog/2bff25d0-c721-4078-8cc9-f3ce6b464428.jpg'
+photo_id = 'AgACAgIAAxkBAAIC4mPoz7KKdPf3q2jGwze9U4__UIysAAL4xTEb-udJSz6xtcwlBJF8AQADAgADeQADLgQ'
 chat_id = 996172963
 
 if __name__=='__main__':
-    pprint(sendPhoto(chat_id=chat_id, photo_url=photo_url))
+    pprint(sendPhoto(chat_id=chat_id, photo=photo_id))
